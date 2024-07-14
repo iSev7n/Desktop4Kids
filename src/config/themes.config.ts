85dc556c0d967a323a58ed4f392d8ecaa520e631
iSev7n <101464244+iSev7n@users.noreply.github.com>
2024-07-14 14:35:13 -0400

    Old Wallpaper / Theme Additions.
    Updated new themes, & removed old wallpapers.

--- src/config/themes.config.ts
+++ src/config/themes.config.ts
@@ -1,8 +1,12 @@
 export const THEMES: Record<number, string | null> = {
 	0: "Dark",
 	1: "Light",
-	2: "Cherry",
+	2: "Pink",
 	3: "Mango",
 	5: "Aqua",
-	6: "Grape"
+	6: "DarkGreen",
+	7: "Green",
+	8: "Cherry",
+	9: "Grape",
+	10: "DeepBlue",
 };
