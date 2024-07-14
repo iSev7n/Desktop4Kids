4c7605cb1402b4c5c519199c9e91981ab76d5018
iSev7n <101464244+iSev7n@users.noreply.github.com>
2024-07-14 11:56:50 -0400

    Update to make.ts
    Updated CMD command for terminal.

--- src/features/apps/terminal/commands/make.ts
+++ src/features/apps/terminal/commands/make.ts
@@ -3,6 +3,10 @@ import { Command } from "../command";
 
 export const make = new Command()
 	.setRequireArgs(true)
+	.setManual({
+        purpose: "Useless Command",
+        usage: "make [options] target"
+    })
 	.setExecute(function(this: Command, args) {
 		if ((args as string[])[0] === "love")
 			return formatError(this.name, "*** No rule to make target 'love'. Stop.");
