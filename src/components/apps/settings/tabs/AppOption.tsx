a3c03ef20ef16bf8e5cfa458bf46b4ac833a39e3
iSev7n <101464244+iSev7n@users.noreply.github.com>
2024-07-24 00:26:06 -0400

    Settings Menu Updated
    Added in slight modifications to settings menu appearance.

--- src/components/apps/settings/tabs/AppOption.tsx
+++ src/components/apps/settings/tabs/AppOption.tsx
@@ -14,70 +14,49 @@ import { App } from "../../../../features/apps/app";
 import { MouseEventHandler } from "react";
 
 interface AppOptionProps {
-	app: App;
-	pins: string[];
-	setPins: Function;
+  app: App;
+  pins: string[];
+  setPins: Function;
 }
 
 export function AppOption({ app, pins, setPins: _setPins }: AppOptionProps) {
-	const isPinned = pins.includes(app.id);
+  const isPinned = pins.includes(app.id);
 
-	const settingsManager = useSettingsManager();
-	const windowsManager = useWindowsManager();
+  const settingsManager = useSettingsManager();
+  const windowsManager = useWindowsManager();
 
-	const { onContextMenu } = useContextMenu({ Actions: (props) =>
-		<Actions {...props}>
-			<ClickAction label="Launch" icon={AppsManager.getAppIconUrl(app.id)} onTrigger={() => windowsManager?.open(app.id)}/>
-			<ClickAction label={isPinned ? "Unpin from taskbar" : "Pin to taskbar"} icon={faThumbTack} onTrigger={() => {
-				const newPins = [...pins];
-				if (isPinned) {
-					removeFromArray(app.id, pins);
-				} else {
-					newPins.push(app.id);
-				}
+  const { onContextMenu } = useContextMenu({
+    Actions: (props) => (
+      <Actions {...props}>
+        <ClickAction label="Launch" icon={AppsManager.getAppIconUrl(app.id)} onTrigger={() => windowsManager?.open(app.id)} />
+        <ClickAction
+          label={isPinned ? "Unpin from taskbar" : "Pin to taskbar"}
+          icon={faThumbTack}
+          onTrigger={() => {
+            const newPins = [...pins];
+            if (isPinned) {
+              removeFromArray(app.id, pins);
+            } else {
+              newPins.push(app.id);
+            }
 
-				const settings = settingsManager?.getSettings(SettingsManager.VIRTUAL_PATHS.taskbar);
-				void settings?.set("pins", newPins.join(","));
-			}}/>
-		</Actions>
-	});
+            const settings = settingsManager?.getSettings(SettingsManager.VIRTUAL_PATHS.taskbar);
+            void settings?.set("pins", newPins.join(","));
+          }}
+        />
+      </Actions>
+    ),
+  });
 
-	return <div className={`${styles.Option} ${styles.OptionHorizontal}`}>
-		<span className={styles.Label}>
-			<ImagePreview className={styles.Icon} source={AppsManager.getAppIconUrl(app.id)}/>
-			{app.name}
-		</span>
-		<button className={styles.IconButton} onClick={onContextMenu as unknown as MouseEventHandler}>
-			<FontAwesomeIcon icon={faEllipsisVertical}/>
-		</button>
-		{/* <div className={styles["Button-group"]}>
-			<Button
-				className={`${styles.Button} ${utilStyles.TextBold}`}
-				onClick={() => windowsManager.open(id)}
-			>
-				Launch
-			</Button>
-			<Button
-				className={`${styles.Button} ${styles["Button-red"]} ${utilStyles.TextBold}`}
-			>
-				Uninstall
-			</Button>
-			<Button
-				className={`${styles.Button} ${utilStyles.TextBold}`}
-				onClick={() => {
-					const newPins = [...pins];
-					if (isPinned) {
-						removeFromArray(id, pins);
-					} else {
-						newPins.push(id);
-					}
-
-					const settings = settingsManager.get(SettingsManager.VIRTUAL_PATHS.taskbar);
-					settings.set("pins", newPins.join(","));
-				}}
-			>
-				{isPinned ? "Unpin from taskbar" : "Pin to taskbar"}
-			</Button>
-		</div> */}
-	</div>;
+  return (
+    <div className={`${styles.Option} ${styles.OptionHorizontal}`} onMouseOver={() => console.log("Hovered")} onClick={() => console.log("Clicked")}>
+      <span className={styles.Label}>
+        <ImagePreview className={styles.Icon} source={AppsManager.getAppIconUrl(app.id)} />
+        {app.name}
+      </span>
+      <button className={styles.IconButton} onClick={onContextMenu as unknown as MouseEventHandler}>
+        <FontAwesomeIcon icon={faEllipsisVertical} />
+      </button>
+    </div>
+  );
 }
